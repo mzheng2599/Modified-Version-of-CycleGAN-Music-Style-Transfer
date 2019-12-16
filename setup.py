3dc14199635f4eb1b29bf6415d59de3e3cf5c44c
mzheng2599 <58921021+mzheng2599@users.noreply.github.com>
2019-12-16 03:38:19 -0800

    Add files via upload
    Setup.py for GCP training

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['tensorflow==1.13.1', 'scipy==1.2.2', 'pretty_midi==0.2.8', 'imageio==2.4.1', 'metrics==0.3.3', ]
+
+setup(
+	name = 'trainer',
+	version = '0.0',
+	install_requires = REQUIRED_PACKAGES,
+	packages = find_packages(),
+	include_package_data = True,
+	description = 'Generic example trainer package.',
+)
